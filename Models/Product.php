f246bea17f8104111543cdc7e190eb44d5917e0e
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-24 16:35:06 +0200

    product class added

---
+++ Models/Product.php
@@ -0,0 +1,51 @@
+<?php
+
+class Product
+{
+    private $img;
+    private $title;
+    private $animal;
+    private $price;
+
+    public function __construct($img, $title, $animal, $price)
+    {
+        $this->setImg($img);
+        $this->setTitle($title);
+        $this->setAnimal($animal);
+        $this->setPrice($price);
+    }
+
+    public function setImg($img)
+    {
+        if (strlen($img)) $this->img = $img;
+    }
+    public function getImg()
+    {
+        return $this->img;
+    }
+    public function setTitle($title)
+    {
+        if (strlen($title)) $this->title = $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setAnimal($animal)
+    {
+        if (strlen($animal)) $this->animal = $animal;
+    }
+    public function getAnimal()
+    {
+        return $this->animal;
+    }
+    public function setPrice($price)
+    {
+        if (!is_numeric($price) || $price <= 0) return false;
+        $this->price = $price;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
