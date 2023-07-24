d234aee64668ed8c62ca6287a044c9d2ffbbb5d2
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-24 16:55:28 +0200

    nutrition class added

---
+++ Models/Nutrition.php
@@ -0,0 +1,33 @@
+<?php
+include_once __DIR__ . '/Product.php';
+
+
+class Nutrition extends Product
+{
+    private $weigth;
+    private $ingredients;
+
+    public function __construct($img, $title, $animal, $price, $weigth, $ingredients)
+    {
+        parent::__construct($img, $title, $animal, $price);
+    }
+
+    public function setWeigth($weigth)
+    {
+        if (!is_numeric($weigth) || $weigth <= 0) return false;
+        $this->weigth = $weigth;
+    }
+
+    public function getWeigth()
+    {
+        return $this->weigth;
+    }
+    public function setIngredients($ingredients)
+    {
+        if (strlen($ingredients)) $this->ingredients = $ingredients;
+    }
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+}
