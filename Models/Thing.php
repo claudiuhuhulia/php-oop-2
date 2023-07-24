6b504c01d6c3625ffbeded7f12fa33b4088d42a1
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-24 17:06:41 +0200

    thing class added

---
+++ Models/Thing.php
@@ -0,0 +1,30 @@
+<?php
+include_once __DIR__ . '/Product.php';
+
+class Thing extends Product
+{
+    private $material;
+    private $dimension;
+
+    public function __construct($img, $title, $animal, $price, $material, $dimension)
+    {
+        parent::__construct($img, $title, $animal, $price);
+    }
+
+    public function setMaterial($material)
+    {
+        if (strlen($material)) $this->material = $material;
+    }
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+    public function setDimension($dimension)
+    {
+        if (strlen($dimension)) $this->dimension = $dimension;
+    }
+    public function getDimension()
+    {
+        return $this->dimension;
+    }
+}
